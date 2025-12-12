c15de2d1539465b20ff098ce1348174f6e8bbb51
AnasEll <anaselhaouss89@gmail.com>
2025-12-13 00:35:01 +0100

    fix some stuffs

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 export const connectDB=async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        const conn = await mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/chat");
         console.log(`MongoDB connected: ${conn.connection.host}`)
     }catch(error){
                 console.log(`MongoDB connected error: ${error}`);
